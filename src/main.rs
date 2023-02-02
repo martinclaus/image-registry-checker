126ef46f32be9a761b0ac1f7b323239add629630
Martin Claus <mclaus@geomar.de>
2023-02-02 20:14:39 +0100

    Add async process to check image slugs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 use serde::Deserialize;
+use tokio::process::Command;
 
 #[get("/health")]
 async fn health() -> impl Responder {
@@ -13,7 +14,28 @@ struct ImageSlug {
 
 #[get("/exists")]
 async fn check_image_exist(info: web::Query<ImageSlug>) -> impl Responder {
-    info.image.clone()
+    let args = info.into_inner();
+    match check_image_slug(args.image).await {
+        Ok(success) => {
+            if success {
+                HttpResponse::Ok()
+            } else {
+                HttpResponse::ExpectationFailed()
+            }
+        }
+        Err(_) => HttpResponse::InternalServerError(),
+    }
+}
+
+async fn check_image_slug(image: impl AsRef<str>) -> std::io::Result<bool> {
+    // spawn process with crane to look up image
+    let mut child = Command::new("crane")
+        .arg("manifest")
+        .arg(image.as_ref())
+        .spawn()
+        .expect("Failed to spawn");
+    let status = child.wait().await?;
+    Ok(status.success())
 }
 
 #[actix_web::main]
@@ -22,4 +44,19 @@ async fn main() -> std::io::Result<()> {
         .bind(("127.0.0.1", 8080))?
         .run()
         .await
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[tokio::test]
+    async fn check_image_slug_returns_true_on_success() {
+        let res = check_image_slug("docker.io/alpine").await;
+
+        assert!(res.is_ok());
+        if let Ok(res) = res {
+            assert!(res)
+        }
+    }
 }
