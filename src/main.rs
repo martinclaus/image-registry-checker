573d9c780b7a3f956381d207a32368c415ab07d8
Martin Claus <mclaus@geomar.de>
2023-02-02 14:50:12 +0100

    Add basic request handler

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use serde::Deserialize;
+
+#[get("/health")]
+async fn health() -> impl Responder {
+    "I'm up and running!"
+}
+
+#[derive(Deserialize)]
+struct ImageSlug {
+    image: String,
+}
+
+#[get("/exists")]
+async fn check_image_exist(info: web::Query<ImageSlug>) -> impl Responder {
+    info.image.clone()
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(health).service(check_image_exist))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
