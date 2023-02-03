990cb8d70e7d876d221d1d026c72f1f451d7fa3b
Martin Claus <mclaus@geomar.de>
2023-02-03 22:38:47 +0100

    Split into components

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,10 @@
 use dotenv::dotenv;
 use log;
-use pretty_env_logger;
-use serde::Deserialize;
-use std::{fmt::Display, process::Stdio};
-use tokio::process::Command;
-use warp::{http::Response, Filter};
-
-#[derive(Deserialize)]
-struct ImageSlug {
-    image: String,
-}
-
-struct LogString<'a, T>(&'a T);
-
-impl<'a> Display for LogString<'a, warp::log::Info<'a>> {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let s = format!(
-            "{} {} ({:.2?}) {}",
-            self.0.method(),
-            self.0.path(),
-            self.0.elapsed(),
-            self.0.status()
-        );
-
-        write!(f, "{}", s)
-    }
-}
-
-#[derive(Clone, Debug)]
-struct ImageChecker {
-    cmd: String,
-}
-
-impl ImageChecker {
-    async fn check_image_slug(&self, image: impl AsRef<str>) -> std::io::Result<bool> {
-        // spawn crane to look up image
-        let mut child = Command::new(&self.cmd)
-            .arg("manifest")
-            .arg(image.as_ref())
-            .stdout(Stdio::null())
-            .stderr(Stdio::null())
-            .spawn()?;
-        let status = child.wait().await?;
-        Ok(status.success())
-    }
-}
+use warp::Filter;
 
 #[tokio::main]
 async fn main() {
-    pretty_env_logger::init_timed();
+    logging::init();
 
     if let Err(e) = dotenv() {
         log::info!("Cannot read environment from .env: {}", e);
@@ -56,47 +12,171 @@ async fn main() {
 
     let args = cli::parse_args();
     let socket_addr = std::net::SocketAddr::new(args.ip(), args.port());
-    let checker = ImageChecker {
-        cmd: args.crane_cmd(),
+    let crane_cmd = args.crane_cmd();
+
+    warp::serve(
+        filter::check_image(crane_cmd)
+            .or(filter::health_check())
+            .with(filter::log()),
+    )
+    .run(socket_addr)
+    .await
+}
+
+mod filter {
+    use crate::{image_exist::ImageChecker, logging::log_func};
+    use serde::Deserialize;
+    use warp::{
+        http::Response,
+        log::{Info, Log},
+        Filter, Rejection, Reply,
     };
 
-    let log = warp::log::custom(|info| match info.status() {
-        code if code.as_u16() >= warp::http::StatusCode::INTERNAL_SERVER_ERROR.as_u16() => {
-            log::error!("{}", LogString(&info))
-        }
-        _ => log::info!("{}", LogString(&info)),
-    });
+    #[derive(Deserialize)]
+    pub struct ImageSlug {
+        pub image: String,
+    }
 
-    let check_image = warp::get()
-        .and(warp::path("exists"))
-        .and(warp::query::<ImageSlug>())
-        .then(move |p: ImageSlug| {
-            let checker = checker.clone();
-            async move {
-                match checker.check_image_slug(p.image).await {
-                    Ok(true) => Response::builder()
-                        .status(warp::http::StatusCode::OK)
-                        .body("ok"),
-                    Ok(false) => Response::builder()
-                        .status(warp::http::StatusCode::NOT_FOUND)
-                        .body("Image does not exist"),
-                    Err(e) => {
-                        log::error!("Spawn of subprocess failed: {}", e);
-                        Response::builder()
-                            .status(warp::http::StatusCode::INTERNAL_SERVER_ERROR)
-                            .body("")
+    pub fn log() -> Log<fn(Info)> {
+        warp::log::custom(log_func)
+    }
+
+    pub fn health_check() -> impl Filter<Extract = (impl Reply,), Error = Rejection> + Clone {
+        warp::get()
+            .and(warp::path("health"))
+            .map(|| Response::builder().body("Ok"))
+    }
+
+    pub fn check_image(
+        cmd: String,
+    ) -> impl Filter<Extract = (impl Reply,), Error = Rejection> + Clone {
+        warp::get()
+            .and(warp::path("exists"))
+            .and(warp::query::<ImageSlug>())
+            .then(move |p: ImageSlug| {
+                let checker = ImageChecker::new(cmd.as_str());
+                async move {
+                    match checker.check_image_slug(p.image).await {
+                        Ok(true) => Response::builder()
+                            .status(warp::http::StatusCode::OK)
+                            .body("ok"),
+                        Ok(false) => Response::builder()
+                            .status(warp::http::StatusCode::NOT_FOUND)
+                            .body("Image does not exist"),
+                        Err(e) => {
+                            log::error!("Spawn of subprocess failed: {}", e);
+                            Response::builder()
+                                .status(warp::http::StatusCode::INTERNAL_SERVER_ERROR)
+                                .body("")
+                        }
                     }
                 }
+            })
+    }
+}
+
+/// Logging
+mod logging {
+    use pretty_env_logger;
+    use std::fmt::Display;
+
+    struct LogString<'a, T>(&'a T);
+
+    impl<'a> Display for LogString<'a, warp::log::Info<'a>> {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            let s = format!(
+                "{} {} ({:.2?}) {}",
+                self.0.method(),
+                self.0.path(),
+                self.0.elapsed(),
+                self.0.status()
+            );
+
+            write!(f, "{}", s)
+        }
+    }
+
+    pub fn log_func(info: warp::log::Info) {
+        match info.status() {
+            code if code.as_u16() >= warp::http::StatusCode::INTERNAL_SERVER_ERROR.as_u16() => {
+                log::error!("{}", LogString(&info))
             }
-        });
+            _ => log::info!("{}", LogString(&info)),
+        }
+    }
 
-    let health_check = warp::get()
-        .and(warp::path("health"))
-        .map(|| Response::builder().body("Ok"));
+    pub fn init() {
+        pretty_env_logger::init_timed();
+    }
+}
 
-    warp::serve(check_image.or(health_check).with(log))
-        .run(socket_addr)
-        .await
+/// Image checker use case
+mod image_exist {
+    use std::process::Stdio;
+    use tokio::process::Command;
+
+    #[derive(Clone, Debug)]
+    pub struct ImageChecker {
+        cmd: String,
+    }
+
+    impl ImageChecker {
+        /// Create ImageChecker
+        pub fn new(cmd: &str) -> Self {
+            Self { cmd: cmd.into() }
+        }
+
+        /// Spawn crane to look up image
+        pub async fn check_image_slug(&self, image: impl AsRef<str>) -> std::io::Result<bool> {
+            let mut child = Command::new(&self.cmd)
+                .arg("manifest")
+                .arg(image.as_ref())
+                .stdout(Stdio::null())
+                .stderr(Stdio::null())
+                .spawn()?;
+            let status = child.wait().await?;
+            Ok(status.success())
+        }
+    }
+
+    #[cfg(test)]
+    mod test {
+        use super::*;
+
+        #[tokio::test]
+        async fn check_image_slug_returns_true_on_success() {
+            let checker = ImageChecker {
+                cmd: "crane".into(),
+            };
+            let res = checker.check_image_slug("docker.io/alpine").await;
+            assert!(res.is_ok());
+            if let Ok(res) = res {
+                assert!(res)
+            }
+        }
+
+        #[tokio::test]
+        async fn check_image_slug_returns_false_on_invalid_slug() {
+            let checker = ImageChecker {
+                cmd: "crane".into(),
+            };
+            let res = checker.check_image_slug("docker.io/non-existent").await;
+            println!("{:?}", res);
+            assert!(res.is_ok());
+            if let Ok(res) = res {
+                assert!(!res)
+            }
+        }
+
+        #[tokio::test]
+        async fn check_image_slug_returns_error_on_failed_spawn() {
+            let checker = ImageChecker {
+                cmd: "not-existent".into(),
+            };
+            let res = checker.check_image_slug("docker.io/non-existent").await;
+            assert!(res.is_err());
+        }
+    }
 }
 
 mod cli {
@@ -141,44 +221,5 @@ mod cli {
     /// Parse CLI args
     pub fn parse_args() -> Args {
         Args::parse()
-    }
-}
-
-#[cfg(test)]
-mod test {
-    use super::*;
-
-    #[tokio::test]
-    async fn check_image_slug_returns_true_on_success() {
-        let checker = ImageChecker {
-            cmd: "crane".into(),
-        };
-        let res = checker.check_image_slug("docker.io/alpine").await;
-        assert!(res.is_ok());
-        if let Ok(res) = res {
-            assert!(res)
-        }
-    }
-
-    #[tokio::test]
-    async fn check_image_slug_returns_false_on_invalid_slug() {
-        let checker = ImageChecker {
-            cmd: "crane".into(),
-        };
-        let res = checker.check_image_slug("docker.io/non-existent").await;
-        println!("{:?}", res);
-        assert!(res.is_ok());
-        if let Ok(res) = res {
-            assert!(!res)
-        }
-    }
-
-    #[tokio::test]
-    async fn check_image_slug_returns_error_on_failed_spawn() {
-        let checker = ImageChecker {
-            cmd: "not-existent".into(),
-        };
-        let res = checker.check_image_slug("docker.io/non-existent").await;
-        assert!(res.is_err());
     }
 }
